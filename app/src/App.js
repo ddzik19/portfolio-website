cd6955014cc2b19a9131411cc75235556d574836
ddzik19 <ddzik09@gmail.com>
2023-01-10 12:31:19 +0000

    navbar 90% complete
    Will need to update the navbar once we have a logo ready.

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,15 @@
 import Layout from "./components/layout";
+import Navbar from "./partials/navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Layout>
         <div>hello</div>
       </Layout>
     </>
-  )
+  );
 }
 
 export default App;
