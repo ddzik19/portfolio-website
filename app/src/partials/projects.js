75ce1391d9d2ba6e397a516053877441d8b7c4a6
ddzik19 <ddzik09@gmail.com>
2023-01-12 17:29:34 +0000

    updated project
    changed project description

--- app/src/partials/projects.js
+++ app/src/partials/projects.js
@@ -2,6 +2,18 @@ import CandyChaser from "../assets/candy-chaser.png";
 import ProjectTempalteLeft from "../components/project-template-left";
 import Fade from "react-reveal/Fade";
 const Projects = () => {
+  const p1 = (
+    <p>
+      This was my first big project that I have completed. After completing my 1
+      year long PLC (Post Leaving Certificate) course I had 3 months before
+      starting college. I decided to develop an Android game to test myself.{" "}
+      <br />
+      <br />
+      In the end I was able to develop and deploy the game to Google Play Store
+      with no knowlegde of C# and UnityEngine.
+    </p>
+  );
+
   return (
     <Fade bottom distance="10%" duration={1500}>
       <div className="padding">
@@ -12,9 +24,7 @@ const Projects = () => {
           <Fade bottom distance="10%" duration={1500}>
             <ProjectTempalteLeft
               title={"Candy Chaser"}
-              desc={
-                "This was my first big project that I have completed. After completing my 1 year long PLC (Post Leaving Certificate) course I had 3 months before starting college. Therefore I decided to stay busy and keep learning. In the end I completed developing the game within 3 months with little knowledge of C# and Unity Engine."
-              }
+              desc={p1}
               image={CandyChaser}
               technology={["C#", "UnityEngine", "OOP", "Android"]}
               fileHref={
