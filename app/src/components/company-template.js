0dbe891228b5e72ec36ba250c4691a7537b6be18
ddzik19 <58696796+ddzik19@users.noreply.github.com>
2023-01-16 12:56:38 +0000

    company list
    Now the company list is mobile responsive

--- app/src/components/company-template.js
+++ app/src/components/company-template.js
@@ -1,14 +1,31 @@
+import useWindowDimensions from "../hooks/use-window-dimensions";
+
 const CompanyTemplate = ({ companyName, website, date, desc }) => {
+    const { width, height } = useWindowDimensions();
     return (
-        <div className="flex">
-            <div className="inline w-[300px] border-r-[2px] border-yellow">
-                <a href={website} className="underline font-semibold text-white text-[20px] hover:text-yellow float-top">{companyName}</a>
-                <p>{date}</p>
-            </div>
-            <div className="inline px-10 py-1 w-full">
-                {desc}
-            </div>
-        </div>
+        <>
+            {width > 1280 ? (
+                <div className="flex">
+                    <div className="inline w-[300px] border-r-[2px] border-yellow">
+                        <a href={website} className="underline font-semibold text-white text-[20px] hover:text-yellow float-top">{companyName}</a>
+                        <p>{date}</p>
+                    </div>
+                    <div className="inline px-10 py-1 w-full">
+                        {desc}
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    <div className=" w-[300px] border-b-[2px] border-yellow">
+                        <a href={website} className="underline font-semibold text-white text-[20px] hover:text-yellow float-top">{companyName}</a>
+                        <p>{date}</p>
+                    </div>
+                    <div className="inline px-10 py-1 w-full">
+                        {desc}
+                    </div>
+                </div>
+            )}
+        </>
     )
 
 }
