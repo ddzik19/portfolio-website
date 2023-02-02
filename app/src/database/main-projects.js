6311ce56be1497760ec283d2113ae8d707d81e5f
ddzik19 <ddzik09@gmail.com>
2023-02-02 14:55:42 +0000

    new project

--- app/src/database/main-projects.js
+++ app/src/database/main-projects.js
@@ -1,5 +1,6 @@
 import CandyChaser from "../assets/candy-chaser.png";
 import Codbot from "../assets/codbot.png";
+import CourseBlogWebsite from "../assets/course-blog-website.png";
 
 export const MainProjects = [
   {
@@ -13,16 +14,39 @@ export const MainProjects = [
   },
   {
     title: "Commander-B",
-    desc: <p>
-      Commander-B is a Discord bot that I have developed for my friends Discord server.
-      When a user enters one of the existing commands, they will get a reply from the bot with the requested information.
-      The bot has multiple commands that can be viewed by entering the !cod.help command.
-      <br />
-      <br />
-      The Discord Bot was hosted on Heroku.com but since Heroku is no longer free, I had to stop hosting the Discord bot.
-    </p>,
+    desc: (
+      <p>
+        Commander-B is a Discord bot that I have developed for my friends
+        Discord server. When a user enters one of the existing commands, they
+        will get a reply from the bot with the requested information. The bot
+        has multiple commands that can be viewed by entering the !cod.help
+        command.
+        <br />
+        <br />
+        The Discord Bot was hosted on Heroku.com but since Heroku is no longer
+        free, I had to stop hosting the Discord bot.
+      </p>
+    ),
     image: Codbot,
     technology: ["Node", "JavaScript", "Discord.js"],
+    githubHref: "https://github.com/ddzik19/cod-bot-wz2",
+    fileHref: "",
+  },
+  {
+    title: "Course Blog Website",
+    desc: (
+      <p>
+        This website is still WIP.
+        <br />
+        <br />
+        On this website I will be posting educational blogs about frontend
+        development. This will be used as a learning experience for me,
+        motivating me to continue learning and sharing my knowledge with
+        everyone.
+      </p>
+    ),
+    image: CourseBlogWebsite,
+    technology: ["React.js", "Sanity", "TailwindCss"],
     githubHref: "https://github.com/ddzik19/cod-bot-wz2",
     fileHref: "",
   },
